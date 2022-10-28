3dfbbaa252fce1082a725a0b411ed3c80db43ac2
Ian <iewing085@gmail.com>
2022-10-28 09:40:39 -0400

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laracasts Video Games</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="/">
+                    <p class="w-32 flex-non">Logo here</p>
+                </a>
+                <ul class="flex">
+                    <li><a href="/games" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</body>
+
+</html>
